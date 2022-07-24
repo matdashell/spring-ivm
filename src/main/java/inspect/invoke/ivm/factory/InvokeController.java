4ef5255b6d8eadbbc5407fc0706bc7a98adbb83e
Mateus Lima Gomes <matdashell729105@gmail.com>
2022-07-24 19:47:58 -0300

    Build

--- src/main/java/inspect/invoke/ivm/factory/InvokeController.java
+++ src/main/java/inspect/invoke/ivm/factory/InvokeController.java
@@ -7,6 +7,7 @@ import inspect.invoke.ivm.dto.InvokeRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.beans.factory.config.NamedBeanHolder;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Configuration
 @RestController
 @RequiredArgsConstructor
 public class InvokeController {
